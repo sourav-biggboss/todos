3b63d7a8e5323bc31f330fbf8bf5f9ed30d914b9
Biggboss <71979923+sourav-biggboss@users.noreply.github.com>
2023-09-07 00:22:19 +0530

    Update tasks.blade.php

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -6,7 +6,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 
     <title>ToDo</title>
@@ -27,12 +26,12 @@
 
         <div class="row mt-3">
             <div class="col-sm-6">
-                <div id="todo-form" action="#" class="needs-validation" novalidate>
+                <div id="todo-form" class="needs-validation" novalidate>
                     <!-- Task Title -->
                     <div class="form-group">
                         <label for="task">Task Title:</label>
                         <input type="text" class="form-control rounded-0" id="title" name="task" required>
-                        <div class="invalid-feedback rounded-0">
+                        <div class="invalid-feedback rounded-0" id="invalid-title">
                             Please enter a Task Title.
                         </div>
                     </div>
@@ -41,10 +40,14 @@
                     <div class="form-group mt-2">
                         <label for="notes">Task Description:</label>
                         <textarea class="form-control rounded-0" id="description" name="notes" rows="4"></textarea>
+                        <div class="invalid-feedback rounded-0" id="invalid-desc">
+                            Please enter a Task Description.
+                        </div>
                     </div>
         
                     <!-- Submit Button -->
                     <button type="submit" id="todo-form-btn" onclick="createTask()" class="btn btn-dark rounded-0 mt-3">Add Task</button>
+                    <button type="button" id="updateToCreate" onclick="updateToCreate()" class="btn btn-danger rounded-0 mt-3" style="display:none;">Add Task</button>
                 </div>
             </div>
             <div class="col-sm-6">
@@ -52,10 +55,10 @@
                     <!-- Nav tabs -->
                     <ul class="nav nav-tabs">
                       <li class="nav-item">
-                        <a class="nav-link active rounded-0" data-bs-toggle="tab" href="#msg">Upcoming Task</a>
+                        <a class="nav-link active rounded-0" data-bs-toggle="tab" href="#msg">Pending Task</a>
                       </li>
                       <li class="nav-item">
-                        <a class="nav-link rounded-0" data-bs-toggle="tab" href="#pro">Pending Task</a>
+                        <a class="nav-link rounded-0" data-bs-toggle="tab" href="#pro">Completed Task</a>
                       </li>
                     </ul>
                     
@@ -104,11 +107,21 @@
     </div>
 
     <script>
-        // $(document).ready(function() {
-            // $(".edit-btn").click(function(event) {
-            //     // editTask();
-            // });
-        // });
+        function updateToCreate (){
+            document.getElementById('todo-form-btn').innerText = 'Add Task';
+            document.getElementById("updateToCreate").style.display = 'none';
+            document.getElementById('title').value = '';
+            document.getElementById('description').value = '';
+            document.getElementById('todo-form-btn').setAttribute('onclick','createTask()');
+        }
+
+        function editBtn (){
+            document.getElementById('todo-form-btn').innerText = 'Edit Task';
+            document.getElementById("updateToCreate").style.display = 'block';
+            document.getElementById('title').value = event.target.parentNode.parentNode.querySelector('h5').innerText;
+            document.getElementById('description').value = event.target.parentNode.parentNode.querySelector('p').innerText;
+            document.getElementById('todo-form-btn').setAttribute('onclick','editTask('+event.target.value+')');
+        };
 
         function completeBtn (){
             updateTaskStatus(event.target.value,'Completed');
@@ -123,6 +136,14 @@
 ;
         // Function to create a new task
         function createTask() {
+            if (document.getElementById('title').value == undefined || document.getElementById('title').value == null || document.getElementById('title').value.trim() == '' || !document.getElementById('title').value) {
+                document.getElementById("invalid-title").style.display = 'block';
+                return;
+            }
+            if (document.getElementById('description').value == undefined || document.getElementById('description').value == null || document.getElementById('description').value.trim() == '' || !document.getElementById('description').value) {
+                document.getElementById("invalid-desc").style.display = 'block';
+                return;
+            }
             const title = document.getElementById('title').value;
             const description = document.getElementById('description').value;
 
@@ -148,11 +169,11 @@
                             </div>
                         `;
 
-                        // Append the HTML to the element with id "task-list"
                         document.getElementById('task-list').insertAdjacentHTML('beforeend', taskHtml);
                         console.log('Task created successfully');
+                        document.getElementById("invalid-title").style.display = 'none';
+                        document.getElementById("invalid-desc").style.display = 'none';
                     } else {
-                        // Handle error, e.g., display validation errors
                         console.error('Error creating task: ' + xhr.responseText);
                     }
                 }
@@ -190,18 +211,15 @@
                             </div>
                         `;
 
-                        // Append the HTML to the element with id "task-list-done"
                         document.getElementById('task-list-done').insertAdjacentHTML('beforeend', taskHtml);
                     } else {
-                        // Handle error, e.g., display error message
                         console.error('Error updating task status: ' + xhr.responseText);
                     }
                 }
             };
 
             const data = {
-                status: newStatus,
-                // _token: csrfToken,
+                status: newStatus
             };
 
             xhr.send(JSON.stringify(data));
@@ -217,16 +235,53 @@
             xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
-                        // Handle success, e.g., remove the task from the list
                         console.log('Task deleted successfully');
                     } else {
-                        // Handle error, e.g., display error message
                         console.error('Error deleting task: ' + xhr.responseText);
                     }
                 }
             };
 
             xhr.send();
+        }
+
+        // Function to update a task
+        function editTask(taskId) {
+            if (document.getElementById('title').value == undefined || document.getElementById('title').value == null || document.getElementById('title').value.trim() == '' || !document.getElementById('title').value) {
+                document.getElementById("invalid-title").style.display = 'block';
+                return;
+            }
+            if (document.getElementById('description').value == undefined || document.getElementById('description').value == null || document.getElementById('description').value.trim() == '' || !document.getElementById('description').value) {
+                document.getElementById("invalid-desc").style.display = 'block';
+                return;
+            }
+            const title = document.getElementById('title').value;
+            const description = document.getElementById('description').value;
+
+            const xhr = new XMLHttpRequest();
+            xhr.open('PUT', `/api/tasks/${taskId}`, true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        const data = JSON.parse(xhr.responseText);
+                        document.querySelector('.edit-btn[value="'+taskId+'"]').parentElement.parentElement.querySelector('h5').innerText = title;
+                        document.querySelector('.edit-btn[value="'+taskId+'"]').parentElement.parentElement.querySelector('p').innerText = description;
+                        document.getElementById("invalid-title").style.display = 'none';
+                        document.getElementById("invalid-desc").style.display = 'none';
+                    } else {
+                        console.error('Error updating task status: ' + xhr.responseText);
+                    }
+                }
+            };
+
+            const data = {
+                title:title,
+                description:description
+            };
+
+            xhr.send(JSON.stringify(data));
         }
 
     </script>
